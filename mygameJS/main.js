1f8d0bb42a6221de14ff2e5c290e8019a003a898
Edward Tanoto <ed27tanoto@gmail.com>
2019-02-03 02:14:20 +0700

    add new project

---
+++ mygameJS/main.js
@@ -0,0 +1,87 @@
+var chars = [
+{ 
+    classes : "King",
+    char: "K",
+    points : 5,
+},
+{
+    classes : "Queen",
+    char: "Q",
+    points : 5,
+},
+
+{
+    classes : "Cavalry",
+    char: "C",
+    points : 5,
+},
+{
+    classes : "Assassin",
+    char: "A",
+    points : 5,
+},
+{
+    classes : "Sniper",
+    char: "S",
+    points : 5,
+},
+{
+    classes : "Pawn",
+    char: "P",
+    points : 1,
+},
+{
+    classes : "Rebel",
+    char: "R",
+    points : 1,
+},
+{
+    classes : "Bomber",
+    char: "B",
+    points : -5,
+},
+]
+
+var playerOneScore = 0;
+var playerTwoScore = 0;
+
+
+function countScore(tes){
+    //if king or queen is together
+
+    var button = document.getElementById("submitId");
+   
+    button.onclick = function() {
+    var value = document.getElementById("submitId").value;
+    console.log(value);
+}
+for(var i=0; i<value.length;i++){
+    console.log(value[i]);
+
+}
+
+
+if(chars.char == K && Q)
+{
+    playerOneScore += 20;
+} 
+
+//if each not together
+else 
+{
+    points == 5
+}
+
+
+
+return 10;
+   
+}
+
+
+
+
+function areaPoints(){
+    //sum all the points within each area
+}
+
