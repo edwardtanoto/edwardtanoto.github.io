0b200342c50a332532fe2f1b359ceb365162cc90
Edward Tanoto <ed27tanoto@gmail.com>
2019-03-16 20:04:07 +0700

    add edu station

---
+++ educationstation/main.js
@@ -0,0 +1,76 @@
+const TypeWriter = function(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+   }
+  
+   //Type Method
+   TypeWriter.prototype.type = function() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+     // Get full text of current word
+    const fullTxt = this.words[current];
+  
+    // Check if deleting
+     if(this.isDeleting) {
+       // Remove char
+       this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+       // Add char
+       this.txt = fullTxt.substring(0, this.txt.length + 1);
+     }
+  
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+  
+     // Initial Type Speed
+     let typeSpeed = 200;
+  
+    if(this.isDeleting) {
+      typeSpeed /= 2;
+     }
+
+  
+     setTimeout(() => this.type(), typeSpeed);
+   };
+    
+    // Init On DOM Load
+    document.addEventListener('DOMContentLoaded', init);
+    
+    // Init App
+    function init() {
+      const txtElement = document.querySelector('.txt-type');
+      const words = JSON.parse(txtElement.getAttribute('data-words'));
+      const wait = txtElement.getAttribute('data-wait');
+      // Init TypeWriter
+      new TypeWriter(txtElement, words, wait);
+    }
+       
+    $(document).ready(function(){
+      // Add smooth scrolling to all links
+      $("a").on('click', function(event) {
+    
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+    
+          // Store hash
+          var hash = this.hash;
+    
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+    
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } 
+      });
+    });
