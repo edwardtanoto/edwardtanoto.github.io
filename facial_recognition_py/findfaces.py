134cc6c323a008121663c0cf8ccab9817b380e1d
Edward Tanoto <ed27tanoto@gmail.com>
2019-04-03 23:23:13 +0700

    facerec

---
+++ facial_recognition_py/findfaces.py
@@ -0,0 +1,8 @@
+import face_recognition
+
+image = face_recognition.load_image_file('./img/myphoto/xa5full.jpg')
+face_locations = face_recognition.face_locations(image)
+
+#print(face_locations)
+
+print(f'There are {len(face_locations)} people in this image') 
